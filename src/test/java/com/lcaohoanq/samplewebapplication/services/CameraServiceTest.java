115f154762f8408cd46fff0bfa712d0e419dcb67
lcaohoanq <hoangclw@gmail.com>
2024-10-13 10:50:41 +0700

    test: add unit test

---
+++ src/test/java/com/lcaohoanq/samplewebapplication/services/CameraServiceTest.java
@@ -0,0 +1,32 @@
+package com.lcaohoanq.samplewebapplication.services;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Disabled
+class CameraServiceTest {
+
+  @Test
+  void findAll() {
+  }
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void findById() {
+  }
+
+  @Test
+  void update() {
+  }
+}
---
+++ src/test/java/com/lcaohoanq/samplewebapplication/services/UserServiceTest.java
@@ -0,0 +1,42 @@
+package com.lcaohoanq.samplewebapplication.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Disabled
+class UserServiceTest {
+
+  @Test
+  void findAll() {
+  }
+
+  @Test
+  void login() {
+  }
+
+  @Test
+  void register() {
+  }
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void findById() {
+  }
+
+  @Test
+  void update() {
+  }
+}
